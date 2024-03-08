e32ed0fbf0d67221fc169e454e1902974829c489
dyeybii <lariza.jl@gmail.com>
2024-03-08 11:26:14 +0800

    first commit

---
+++ lib/login_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:driver/reusable_widgets.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController plateNumberController = TextEditingController();
+  TextEditingController birthdayController = TextEditingController();
+  bool _isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          CustomColumnWithLogo(), // Using the custom column with logo from reusable_widgets.dart
+          Positioned(
+            top: 100,
+            left: 30,
+            right: 30,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Sign in with email or phone number.",
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 26,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 20),
+                customTextField(
+                  "Plate Number",
+                  Icons.directions_car,
+                  false,
+                  plateNumberController,
+                  inputType: TextInputType.text, // Allowing letters and numbers
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: birthdayController,
+                  obscureText: false,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  cursorColor: const Color.fromARGB(255, 19, 19, 19),
+                  style: const TextStyle(color: Color.fromARGB(255, 14, 13, 13)),
+                  decoration: InputDecoration(
+                    labelText: "Birthdate (MMDDYYYY)",
+                    prefixIcon: Icon(Icons.calendar_today, color: const Color.fromARGB(179, 40, 39, 39)),
+                    border: const OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number, // Only allowing numbers
+                ),
+                const SizedBox(height: 30),
+                signInSignUpButton(context, true, () {
+                  // Implement your login logic here
+                }),
+              ],
+            ),
+          ),
+          Positioned(
+            left: 0,
+            bottom: -10,
+            child: logowidget("assets/images/LOGO.png"), // Inserting the logo
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/reusable_widgets.dart
@@ -0,0 +1,90 @@
+
+// reusable_widgets.dart
+import 'package:flutter/material.dart';
+
+Image logowidget(String imageName) {
+  return Image.asset(
+    imageName,
+    fit: BoxFit.fitWidth,
+  );
+}
+
+TextField customTextField(String labelText, IconData prefixIcon, bool isPasswordType, TextEditingController controller, {bool obscureText = false, TextInputType? inputType}) {
+  bool _isPasswordVisible = obscureText; // Track password visibility
+
+  return TextField(
+    controller: controller,
+    obscureText: _isPasswordVisible, // Use the tracked variable for password visibility
+    enableSuggestions: !isPasswordType,
+    autocorrect: !isPasswordType,
+    cursorColor: const Color.fromARGB(255, 19, 19, 19),
+    style: const TextStyle(color: Color.fromARGB(255, 14, 13, 13)),
+    decoration: InputDecoration(
+      labelText: labelText,
+      prefixIcon: Icon(prefixIcon, color: const Color.fromARGB(179, 40, 39, 39)),
+      suffixIcon: obscureText ? IconButton(
+        icon: Icon(
+          _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+        ),
+        onPressed: () {
+          _isPasswordVisible = !_isPasswordVisible; // Toggle password visibility state
+        },
+      ) : null,
+      border: const OutlineInputBorder(),
+    ),
+    keyboardType: inputType, // Set the keyboardType to the provided inputType
+  );
+}
+
+
+Container signInSignUpButton(BuildContext context, bool isLogin, Function onTap) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 50,
+    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(90),
+    ),
+    child: ElevatedButton(
+      onPressed: () {
+        onTap();
+      },
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 10), backgroundColor: const Color(0xFF2E3192),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+      child: Text(
+        isLogin ? 'LOG IN' : 'SIGN UP',
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ),
+      ),
+    ),
+  );
+}
+
+class CustomColumnWithLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Expanded(
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
