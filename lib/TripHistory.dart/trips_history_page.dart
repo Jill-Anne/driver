963a781dfaa7a8d9005df908d841617b41d38649
Jill <jillannebragado13@gmail.com>
2024-09-20 01:38:27 +0800

    trip history ui

--- lib/TripHistory.dart/trips_history_page.dart
+++ lib/TripHistory.dart/trips_history_page.dart
@@ -3,25 +3,99 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart'; // Import intl package for date formatting
-
 class TripsHistoryPage extends StatefulWidget {
   const TripsHistoryPage({super.key});
-
   @override
   State<TripsHistoryPage> createState() => _TripsHistoryPageState();
 }
-
 class _TripsHistoryPageState extends State<TripsHistoryPage> {
   final completedTripRequestsOfCurrentDriver =
       FirebaseDatabase.instance.ref().child("tripRequests");
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          // Content with ListView
-          StreamBuilder(
+       String currentDriverTotalTripsCompleted = "";
+
+  getCurrentDriverTotalNumberOfTripsCompleted() async {
+    DatabaseReference tripRequestsRef =
+        FirebaseDatabase.instance.ref().child("tripRequests");
+
+    await tripRequestsRef.once().then((snap) async {
+      if (snap.snapshot.value != null) {
+        Map<dynamic, dynamic> allTripsMap = snap.snapshot.value as Map;
+        int allTripsLength = allTripsMap.length;
+
+        List<String> tripsCompletedByCurrentDriver = [];
+
+        allTripsMap.forEach((key, value) {
+          if (value["status"] != null) {
+            if (value["status"] == "ended") {
+              if (value["driverID"] == FirebaseAuth.instance.currentUser!.uid) {
+                tripsCompletedByCurrentDriver.add(key);
+              }
+            }
+          }
+        });
+
+        setState(() {
+          currentDriverTotalTripsCompleted =
+              tripsCompletedByCurrentDriver.length.toString();
+        });
+      }
+    });
+  }
+
+    @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    getCurrentDriverTotalNumberOfTripsCompleted();
+  }
+
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: Column(
+      children: [
+        // Row for Recent Rides on the left and Total Trips on the right (fixed header)
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Recent Rides',
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 1, 42, 123),
+                ),
+              ),
+              Container(
+                width: 140, // Specify width for the rectangle
+                height: 40, // Specify height for the rectangle
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[800]!, width: 1),
+                  borderRadius: BorderRadius.circular(3),
+                  color: Color.fromARGB(255, 1, 42, 123),
+                ),
+                alignment: Alignment.center, // Center the text inside the container
+                child: Text(
+                  'Total Trips: $currentDriverTotalTripsCompleted',
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        // Scrollable content
+        Expanded(
+          child: StreamBuilder(
             stream: completedTripRequestsOfCurrentDriver.onValue,
             builder: (BuildContext context, snapshotData) {
               if (snapshotData.hasError) {
@@ -33,7 +107,6 @@ class _TripsHistoryPageState extends State<TripsHistoryPage> {
                   ),
                 );
               }
-
               if (!(snapshotData.hasData) ||
                   snapshotData.data?.snapshot.value == null) {
                 print("No data available or snapshot is null.");
@@ -46,39 +119,15 @@ class _TripsHistoryPageState extends State<TripsHistoryPage> {
               }
 
               Map dataTrips = snapshotData.data!.snapshot.value as Map;
-              print("Data received from Firebase: $dataTrips");
-
               List<Map> tripsList = [];
               dataTrips.forEach((key, value) {
-                print("Processing trip with key: $key and value: $value");
                 tripsList.add({"key": key, ...value});
               });
-
-              // Check if no trips are found
               if (tripsList.isEmpty) {
-                print("No trips found for the current driver.");
                 return const Center(
                   child: Text(
                     "No completed trips found.",
                     style: TextStyle(color: Colors.white),
-                  ),
-                );
-              }
-
-              // Check if there are pending trips (status not 'ended')
-              List<Map> pendingTrips = tripsList
-                  .where((trip) =>
-                      trip["status"] != "ended" &&
-                      trip["driverID"] ==
-                          FirebaseAuth.instance.currentUser!.uid)
-                  .toList();
-
-              if (pendingTrips.isEmpty) {
-                print("No pending trips found for the current driver.");
-                return const Center(
-                  child: Text(
-                    "No Trip Records Available",
-                    style: TextStyle(color: Colors.black87, fontSize: 18),
                   ),
                 );
               }
@@ -88,21 +137,14 @@ class _TripsHistoryPageState extends State<TripsHistoryPage> {
               for (var trip in tripsList) {
                 if (trip["status"] == "ended" &&
                     trip["driverID"] == FirebaseAuth.instance.currentUser!.uid) {
-                  // Parse and format the trip ended time
                   if (trip["tripEndedTime"] != null) {
                     try {
                       DateFormat dateFormat = DateFormat("MMMM d, yyyy h:mm a");
-                      DateTime tripEndedDateTime =
-                          dateFormat.parse(trip["tripEndedTime"]);
-                      String formattedDate =
-                          DateFormat('MM/dd/yyyy').format(tripEndedDateTime);
-
-                      // Initialize the list if not already present
+                      DateTime tripEndedDateTime = dateFormat.parse(trip["tripEndedTime"]);
+                      String formattedDate = DateFormat('MM/dd/yyyy').format(tripEndedDateTime);
                       if (!groupedTrips.containsKey(formattedDate)) {
                         groupedTrips[formattedDate] = [];
                       }
-
-                      // Add trip to the corresponding date list
                       groupedTrips[formattedDate]!.add(trip);
                     } catch (e) {
                       print("Date parsing error: $e");
@@ -113,92 +155,97 @@ class _TripsHistoryPageState extends State<TripsHistoryPage> {
 
               // Sort the dates in descending order
               List<String> sortedDates = groupedTrips.keys.toList()
-                ..sort((a, b) => DateFormat('MM/dd/yyyy')
-                    .parse(b)
-                    .compareTo(DateFormat('MM/dd/yyyy').parse(a)));
-
+                ..sort((a, b) => DateFormat('MM/dd/yyyy').parse(b).compareTo(DateFormat('MM/dd/yyyy').parse(a)));
               return ListView.builder(
                 shrinkWrap: true,
                 itemCount: sortedDates.length,
                 itemBuilder: (context, dateIndex) {
                   String date = sortedDates[dateIndex];
                   List<Map> tripsForDate = groupedTrips[date]!;
-
                   return Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Padding(
                         padding: const EdgeInsets.only(left: 15, bottom: 4),
+                        ///DISPLAY OF DATE 
                         child: Text(
-                          //'On $date (${tripsForDate.length} trips)',
                           '$date ',
                           style: const TextStyle(
-                            fontSize: 18,
+                            fontSize: 14,
                             fontWeight: FontWeight.bold,
-                            color: Colors.black87,
+                           color: Color.fromARGB(255, 1, 42, 123),
                           ),
                         ),
                       ),
                       ...tripsForDate.map((trip) {
                         String tripEndedTimeFormatted = "N/A";
-
                         if (trip["tripEndedTime"] != null) {
                           try {
-                            DateFormat dateFormat =
-                                DateFormat("MMMM d, yyyy h:mm a");
-                            DateTime tripEndedDateTime =
-                                dateFormat.parse(trip["tripEndedTime"]);
-                            tripEndedTimeFormatted = DateFormat(
-                                    'MMMM d, yyyy h:mm a')
-                                .format(tripEndedDateTime);
+                            DateFormat dateFormat = DateFormat("MMMM d, yyyy h:mm a");
+                            DateTime tripEndedDateTime = dateFormat.parse(trip["tripEndedTime"]);
+                            tripEndedTimeFormatted = DateFormat('MMMM d, yyyy h:mm a').format(tripEndedDateTime);
                           } catch (e) {
                             tripEndedTimeFormatted = "Invalid date format";
                             print("Date parsing error: $e");
                           }
                         }
 
-                        return Card(
-                          color: Colors.white,
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            side: BorderSide(
-                                color: Colors.grey[800]!, width: 1),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: ListTile(
-                            contentPadding: const EdgeInsets.symmetric(
-                                horizontal: 12, vertical: 12),
-                            title: Text(
-                              'Trip on $tripEndedTimeFormatted\nFare: PHP ${trip["fareAmount"]}',
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black87,
+                        return Column(
+                          children: [
+                            Card(
+                          
+                              color: Colors.white,
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
                               ),
-                            ),
-                            subtitle: Text(
-                              'From ${trip["pickUpAddress"]} to ${trip["dropOffAddress"]}',
-                              style: const TextStyle(
-                                fontSize: 14,
-                                color: Colors.black54,
-                              ),
-                            ),
-                            trailing: const Icon(
-                              Icons.arrow_forward_ios,
-                              size: 16,
-                              color: Colors.grey,
-                            ),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => TripDetailsPage(
-                                    tripDetails: trip,
+                              child: ListTile(
+                                leading: Image.asset(
+                                  'assets/images/trisikol.png',
+                                  width: 50, // Set width of the image
+                                  height: 50, // Set height of the image
+                                  fit: BoxFit.contain, // Fit the image
+                                ),
+                                contentPadding: const EdgeInsets.symmetric(horizontal: 12, vertical: 3),
+                                title: Text(
+                                  'Trip on $tripEndedTimeFormatted\nFare: ₱${trip["fareAmount"]}',
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black87,
                                   ),
                                 ),
-                              );
-                            },
-                          ),
+                                subtitle: Text(
+                                  'From ${trip["pickUpAddress"]} to ${trip["dropOffAddress"]}',
+                                  style: const TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black54,
+                                  ),
+                                ),
+                                trailing: const Icon(
+                                  Icons.arrow_forward_ios,
+                                  size: 16,
+                                  color: Colors.grey,
+                                ),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => TripDetailsPage(
+                                        tripDetails: trip,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            Divider(
+                              thickness: 2, // Adjust thickness
+                              color: Colors.grey[400], // Adjust color
+                              indent: 20, // Adjust the left padding of the divider
+                              endIndent: 20, // Adjust the right padding of the divider
+                            ),
+                          ],
                         );
                       }).toList(),
                     ],
@@ -207,8 +254,10 @@ class _TripsHistoryPageState extends State<TripsHistoryPage> {
               );
             },
           ),
-        ],
-      ),
-    );
-  }
+        ),
+      ],
+    ),
+  );
+}
+
 }
--- lib/pages/trips_page.dart
+++ lib/pages/trips_page.dart
@@ -21,84 +21,102 @@ class _TripsPageState extends State<TripsPage> {
     });
   }
 
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Column(
-      children: [
-        // Add space at the top
-        SizedBox(height: 30),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // Add space at the top
+          SizedBox(height: 30),
 
-        // Tab bar with background color
-        Container(
-          height: 50,
-          color: Color(0xFFF2F8FC), // Set the background color of the tab bar
-          padding: const EdgeInsets.symmetric(vertical: 0), // Adjust padding as needed
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Pending Booking Tab
-              GestureDetector(
-                onTap: () => onTabSelected(1),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: selectedIndex == 1 ? Colors.deepPurple : Colors.white,
-                        width: 2.0,
+          // Tab bar with background color
+          Container(
+            height: 50,
+            color: Color.fromARGB(255, 1, 42,
+                123), // Color(0xFFF2F8FC), // Set the background color of the tab bar
+            padding: const EdgeInsets.symmetric(
+                vertical: 0), // Adjust padding as needed
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Pending Booking Tab
+                GestureDetector(
+                  onTap: () => onTabSelected(1),
+                  child: Container(
+                    height: 50,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 30),
+                    decoration: BoxDecoration(
+                      color: selectedIndex == 0
+                          ? Color.fromARGB(255, 1, 42, 123)
+                          : Color.fromARGB(255, 1, 42, 123), // Background color
+                      border: Border(
+                        bottom: BorderSide(
+                          color: selectedIndex == 1
+                              ? Color.fromARGB(255, 1, 42, 123)
+                              : Color.fromARGB(255, 1, 42, 123),
+                          width: 3.0,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      'Pending Service',
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: selectedIndex == 1 ? Colors.white : Colors.grey,
+                        fontWeight: selectedIndex == 1
+                            ? FontWeight.bold
+                            : FontWeight.normal,
                       ),
                     ),
                   ),
-                  child: Text(
-                    'Pending Service',
-                    style: TextStyle(
-                      fontSize: 17,
-                      color: selectedIndex == 1 ? Colors.deepPurple : Colors.grey,
-                      fontWeight: selectedIndex == 1 ? FontWeight.bold : FontWeight.normal,
-                    ),
-                  ),
                 ),
-              ),
 
-              // Trip History Tab
-              GestureDetector(
-                onTap: () => onTabSelected(0),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-                  decoration: BoxDecoration(
-                    border: Border(
-                      bottom: BorderSide(
-                        color: selectedIndex == 0 ? Colors.deepPurple : Colors.white,
-                        width: 2.0,
+                // Trip History Tab
+                GestureDetector(
+                  onTap: () => onTabSelected(0),
+                  child: Container(
+                    height: 50,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 30),
+                    decoration: BoxDecoration(
+                      color: selectedIndex == 0
+                          ? Color.fromARGB(255, 1, 42, 123)
+                          : Color.fromARGB(255, 1, 42, 123), // Background color
+                      border: Border(
+                        bottom: BorderSide(
+                          color:
+                              selectedIndex == 0 ? Color.fromARGB(255, 1, 42, 123): Color.fromARGB(255, 1, 42, 123),
+                          width: 3.0,
+                        ),
+                      ),
+                    ),
+                    child: Text(
+                      'Trip History',
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: selectedIndex == 0 ?Colors.white : Colors.grey,
+                        fontWeight: selectedIndex == 0
+                            ? FontWeight.bold
+                            : FontWeight.normal,
                       ),
                     ),
                   ),
-                  child: Text(
-                    'Trip History',
-                    style: TextStyle(
-                      fontSize: 17,
-                      color: selectedIndex == 0 ? Colors.deepPurple : Colors.black54,
-                      fontWeight: selectedIndex == 0 ? FontWeight.bold : FontWeight.normal,
-                    ),
-                  ),
                 ),
-              ),
 
-              SizedBox(width: 20), // Space between the two tabs
-            ],
+                SizedBox(width: 20), // Space between the two tabs
+              ],
+            ),
           ),
-        ),
 
-        // Content changes based on selected tab
-        Expanded(
-          child: selectedIndex == 0
-              ? TripsHistoryPage() // Show Trip History content
-              : NewAdvanceBooking(), // Show Pending Booking content
-        ),
-      ],
-    ),
-  );
-}
-
+          // Content changes based on selected tab
+          Expanded(
+            child: selectedIndex == 0
+                ? TripsHistoryPage() // Show Trip History content
+                : NewAdvanceBooking(), // Show Pending Booking content
+          ),
+        ],
+      ),
+    );
+  }
 }
